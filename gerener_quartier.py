9052a94b53d1e7ef7aafd76089e462eacca1b125
Etienne <etienne.leclerc@polymtl.ca>
2024-02-03 16:08:10 -0500

    added code louis and interface graphique

---
+++ gerener_quartier.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+
+#theta est en degres, superficie en m^2
+def centeredAngledElliptic(theta, eccentricite, superficie):
+    theta = np.radians(theta)
+    print(theta)
+    phi = np.arange(0, 2*np.pi, 0.00001)
+    a = (superficie/(np.pi*(1-eccentricite**2)**(1/2)))**(1/2)
+    b = a*(1-eccentricite**2)**(1/2)
+    
+    r = b / (1-(eccentricite**2)*(np.cos(phi)**2))**(1/2)
+    
+    x = r*np.cos(phi)*np.cos(theta) - r*np.sin(phi)*np.sin(theta)
+    y = r*np.sin(phi)*np.cos(theta) + r*np.cos(phi)*np.sin(theta)
+    
+    
+    return x, y, a, superficie 
+
+theta, eccentricite, superficie = 120, 0.95, 45000
+x, y, a, superficie = centeredAngledElliptic(theta, eccentricite, superficie)
+plt.plot(x, y, "-r")
+plt.xlim(-1*a, a)
+plt.ylim(-1*a, a)
+plt.axhline(ls=":", c="k")
+plt.axvline(ls=":", c="k")
+plt.show()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from batiment import batiment
 from maison import maison
 import numpy as np
+import plotly.graph_objects as go
 
 #Parametres de generation
 nb_maisons_needed = 24
@@ -12,8 +13,8 @@ nb_maisons = 0
 
 for row in range(map.shape[0]):
     for col in range(map.shape[1]):
-        if np.random.rand()>.5 and nb_maisons<nb_maisons_needed:
-            map[row, col] = maison(couleur="Bleue", nombre_chambres=3, superficie=150, lettre="M")
+        if np.random.rand()>.8 and nb_maisons<nb_maisons_needed:
+            map[row, col] = maison(couleur="red", nombre_chambres=3, superficie=150, lettre="M")
             nb_maisons=nb_maisons+1
 
 def affichage_map_avec_lettres(map):
@@ -27,3 +28,37 @@ def affichage_map_avec_lettres(map):
 
 # Call the function to print the matrix
 affichage_map_avec_lettres(map)
+
+# Fonction pour creer forme de carre
+def creer_carres(center, size):
+    x = [center[0] - size / 2, center[0] + size / 2, center[0] + size / 2, center[0] - size / 2, center[0] - size / 2]
+    y = [center[1] - size / 2, center[1] - size / 2, center[1] + size / 2, center[1] + size / 2, center[1] - size / 2]
+    return x, y
+
+# Creer des carres
+squares = []
+for i in range(map.shape[0]):
+    for j in range(map.shape[1]):
+        x, y = creer_carres(center=(i, j), size=1)
+        if map[i, j] != None:
+            square = go.Scatter(x=x, y=y, mode='lines',
+                            line=dict(color='black'),
+                            fill='toself',
+                            fillcolor=map[i, j].get_couleur(),
+                            hoverinfo='skip')
+        else:
+            square = go.Scatter(x=x, y=y, mode='lines',
+                            line=dict(color='black'),
+                            fill='toself',
+                            fillcolor='lightblue',
+                            hoverinfo='skip')            
+        squares.append(square)
+
+# Creer fichier
+fig = go.Figure(squares)
+
+# Mettre a jour le layout
+fig.update_layout(showlegend=False)
+
+# Montrer la figure
+fig.show()
