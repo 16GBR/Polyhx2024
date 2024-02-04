8527251154b00f4dd09f78502f08470c6f771b3b
JeremiePlam <jeremieplam@protonmail.com>
2024-02-04 08:37:02 -0500

    Updated with new main

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def index():
     return render_template("index.html")
 
 
-from app_packages.test import test
+from app_packages import main, zoning
 import numpy as np
 
 @app.route('/process_form', methods=['POST'])
@@ -18,14 +18,14 @@ def process_form():
     community = request.form['community']
     parks = request.form['parks']
 
-    print(f"Form submitted with values: Width={width}, Population={population}, Commercial={commercial}, Community={community}, Parks={parks}")
-    '''print(blocsAPlacer(
+    #print(f"Form submitted with values: Width={width}, Population={population}, Commercial={commercial}, Community={community}, Parks={parks}")
+    main.main(zoning.blocsAPlacer(
         np.ones((int(width), int(width))), 
         int(population), 
         float(commercial)/100, 
         float(community)/100, 
         float(parks)/100, 
-        10, 20))'''
+        10, 20))
     return render_template("index.html", width=width, population=population, commercial=commercial, community=community, parks=parks)
 
 if __name__ == '__main__':
